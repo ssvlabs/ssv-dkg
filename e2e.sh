1f551cf5f60916b81c30dd853819e0bd8bd400e4
pavelkrolevets <pavelkrolevets@gmail.com>
2024-04-02 13:04:21 +0300

    add 1, 10, 100 validators to e2e

--- e2e.sh
+++ e2e.sh
@@ -12,8 +12,10 @@ VESRION=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='ver
 env GO111MODULE=on go install -v -ldflags "-X main.Version=$VESRION" cmd/ssv-dkg/ssv-dkg.go
 
 # run init ceremony
-ssv-dkg init \
-          --validators 10 \
+
+for val in 1 10 100; do 
+  ssv-dkg init \
+          --validators $val \
           --operatorIDs 1,2,3,4 \
           --operatorsInfo '[{"id": 1,"public_key": "LS0tLS1CRUdJTiBSU0EgUFVCTElDIEtFWS0tLS0tCk1JSUJJakFOQmdrcWhraUc5dzBCQVFFRkFBT0NBUThBTUlJQkNnS0NBUUVBMVg2MUFXY001QUNLaGN5MTlUaEIKby9HMWlhN1ByOVUralJ5aWY5ZjAyRG9sd091V2ZLLzdSVUlhOEhEbHBvQlVERDkwRTVQUGdJSy9sTXB4RytXbwpwQ2N5bTBpWk9UT0JzNDE5bEh3TzA4bXFja1JsZEg5WExmbmY2UThqWFR5Ym1yYzdWNmwyNVprcTl4U0owbHR1CndmTnVTSzNCZnFtNkQxOUY0aTVCbmVaSWhjRVJTYlFLWDFxbWNqYnZFL2cyQko4TzhaZUgrd0RzTHJiNnZXQVIKY3BYWG1uelE3Vlp6ZklHTGVLVU1CTTh6SW0rcXI4RGZ4SEhSeVU1QTE3cFU4cy9MNUp5RXE1RGJjc2Q2dHlnbQp5UE9BYUNzWldVREI3UGhLOHpUWU9WYi9MM1lnSTU4bjFXek5IM0s5cmFreUppTmUxTE9GVVZzQTFDUnhtQ2YzCmlRSURBUUFCCi0tLS0tRU5EIFJTQSBQVUJMSUMgS0VZLS0tLS0K","ip": "http://35.161.207.60:3030"},
                             {"id": 2,"public_key": "LS0tLS1CRUdJTiBSU0EgUFVCTElDIEtFWS0tLS0tCk1JSUJJakFOQmdrcWhraUc5dzBCQVFFRkFBT0NBUThBTUlJQkNnS0NBUUVBeUtVWTVEUmZZREljengzcjhVY0UKTlpFMFdIQXFuV2FIRjZYRlUydVdObjVOVE94Zkt4ZmZaLzkyeVE1citQVkJPRmQrcHhILzI2QXJVT3dNL1lBRQpRbDZ0VzBtc1FqdUtIU1Q4aUtvTDRTNUt0aDNoeTBqeFRHR1ZZaWdjWG1vRURjd2YxaG8wdWRxRmlEN3dFWXN1CmZHa2E2U1ZQNnBab1NMaU9HZFRKUWVzVDI5WEVCdDZnblhMaFB1MER2K0xsQUJJQ1pqWEFTZWtpSFVKUHRjYlgKRjZFL0lScGpkWHVNSmUyOXZDcmZudXhWWk93a1ptdzJXdGljYlNDOVJpSFRYWUQ1dnVGakZXRHNZMERHUDhzOAoyc1haVHdsNWl4dEhlUWM2N1lLRFN6YU1MNnY1VUVZblhUTzZzNHFVSWVnTXJwZjd3S0xGVWxqRTMwbnNIaVBUCjBRSURBUUFCCi0tLS0tRU5EIFJTQSBQVUJMSUMgS0VZLS0tLS0K","ip": "http://35.88.187.25:3030"},
@@ -29,19 +31,23 @@ ssv-dkg init \
           --logLevelFormat capitalColor \
           --logFilePath $(pwd)/output/debug.log
 
-# verify all ceremony results
-find $(pwd)/output -mindepth 1 -maxdepth 1 -type d -name "ceremony*" | while read fname; do
-  echo "$fname" 
-  if ssv-dkg verify \
-        --ceremonyDir $fname \
-        --nonce 1 \
-        --owner 0x81592c3de184a3e2c0dcb5a261bc107bfa91f494 \
-        --validators 10 \
-        --withdrawAddress 0xa1a66cc5d309f19fb2fda2b7601b223053d0f7f4; then
-    echo results verify success
-  else
-    exit 1
-  fi
+  # verify all ceremony results
+  find $(pwd)/output -mindepth 1 -maxdepth 1 -type d -name "ceremony*" | while read fname; do
+    echo "$fname" 
+    if ssv-dkg verify \
+          --ceremonyDir $fname \
+          --nonce 1 \
+          --owner 0x81592c3de184a3e2c0dcb5a261bc107bfa91f494 \
+          --validators $val \
+          --withdrawAddress 0xa1a66cc5d309f19fb2fda2b7601b223053d0f7f4; then
+      echo results verify success
+    else
+      exit 1
+    fi
+    rm -rf $(pwd)/output/ceremony*
+  done
 done
+
+
  
 
