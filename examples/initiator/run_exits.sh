b6def324a5c322de2b38fd00ce18dc9eac592c94
Matus Kysel <matus@blox.io>
2024-04-15 14:10:39 +0200

    fix uplaod script

--- examples/initiator/run_exits.sh
+++ examples/initiator/run_exits.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
 
+# Directory containing the exit files
+exit_dir="$(pwd)/output"
 
-# run all exits in the folder 
-find $(pwd)/output -mindepth 1 -maxdepth 1 -type f -name "validator-exit-*.json" | while read fname; do
+# Find and process each exit file in the specified directory
+find "${exit_dir}" -mindepth 1 -maxdepth 1 -type f -name "validator-exit-*.json" | while read -r fname; do
     echo "Executing $fname"
-    epoch= jq .message.epoch $fname
-    validator_index= jq .message.validator_index $fname
-    signature= jq .signature $fname
+    
+    # Extract values using jq and remove quotes
+    epoch=$(jq -r '.message.epoch' "$fname")
+    validator_index=$(jq -r '.message.validator_index' "$fname")
+    signature=$(jq -r '.signature' "$fname")
 
-    if curl -X 'POST' \  
+    # Create the JSON payload
+    json_payload=$(cat <<-EOF
+    {
+        "message": {
+            "epoch": "${epoch}",
+            "validator_index": "${validator_index}"
+        },
+        "signature": "${signature}"
+    }
+EOF
+    )
+
+    # Send the data using curl
+    if ! curl -X 'POST' \
         'http://bn-h-3.stage.bloxinfra.com:3500/eth/v1/beacon/pool/voluntary_exits' \
         -H 'accept: */*' \
         -H 'Content-Type: application/json' \
-        -d '{
-        "message": {
-            "epoch": '$epoch',
-            "validator_index": '$validator_index'
-        },
-        "signature": '$signature'
-        }'
+        -d "$json_payload"
     then
-        exit 1
+        echo "Failed to send data for $fname"
+        # Optionally, you can choose to exit or continue based on requirements
+        # exit 1
     fi
 done
